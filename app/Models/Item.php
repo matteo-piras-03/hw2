6e127e06b3ff6dde97f98a88856caf9832af27ae
matteo-piras-03 <piras.unict@gmail.com>
2025-06-16 21:25:03 +0200

    Update Item.php

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -8,7 +8,4 @@ class Item extends Model
 {
     protected $table = "item";
     public $timestamps = false;
-    public function cart(){
-        return $this->belongstoMany(User::class,"cart","item_id","user_id");
-    }
 }
